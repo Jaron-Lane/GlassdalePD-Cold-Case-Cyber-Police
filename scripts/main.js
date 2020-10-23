9da7b0efac77263414367d3e7eefbae9d5017d8b
Jaron-Lane <jaronwohlwend@icloud.com>
2020-10-23 14:25:34 -0500

    built and exported useOfficers and getOfficers

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
 console.log("Welcome to the main module")
 
+import { getOfficers } from "./officers/OfficerProvider.js"
 import { CriminalList } from "./criminals/CriminalList.js"
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js"
 
 ConvictionSelect()
 
 CriminalList()
+
+getOfficers()
