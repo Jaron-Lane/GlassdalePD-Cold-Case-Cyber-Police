c8a3e4d31421fda0facf848decd64fd67b716584
Jaron-Lane <jaronwohlwend@icloud.com>
2020-10-27 09:22:35 -0500

    refoctered and finished officer dropdown menu

--- scripts/officers/OfficerList.js
+++ scripts/officers/OfficerList.js
@@ -11,6 +11,7 @@ const officerContent = document.querySelector(".filters__officer")
 export const OfficerList = () => {
     getOfficers().then(() => {
         const appStateOfficers = useOfficers()
+        console.log(appStateOfficers)
         render(appStateOfficers)
     })
 }
@@ -29,41 +30,6 @@ const render = (officerCollection) => {
 }
 
 
-
-// Listen for the custom event you dispatched in OfficerSelect
-
-
-    // Use the property you added to the event detail.
-
-
-        //Get the list of officers
-
-
-        // console.log("array of officers", officersArray)
-
-        /* 
-        We have the the id of the officer that the user selected from the drop down. But each officer object has the name of the crime they were convicted for. So we need to get the name of the conviction associated with the unique identifier. 
-        */
-
-        // Get the array of officers
-
-
-        // Use the find method to get the first object in the officerss array that has the same id as the id of the chosen crime
-
-
-        // console.log("convictionThatWasChosen", convictionThatWasChosen)
-
-        /*
-        Now that we have the name of the chosen officeer, filter the officerss application state down to the people that arrested the criminal
-        */
-
-
-        // console.log("filteredOfficersArray", filteredOfficersArray)
-
-    /*
-      Then invoke render() and pass the filtered collection as
-      an argument
-    */
 
 
     // review lines 68-115 of criminal list to review 
